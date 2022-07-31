7da36813a3e23b911492c021adf3f3f02fcd227a
silver <silver36c@gmail.com>
2022-08-01 03:13:04 +0900

    FEAT: posts 레포지터리 paging, hateos 추가

--- src/main/java/conatus/domain/posts/PostsRepository.java
+++ src/main/java/conatus/domain/posts/PostsRepository.java
@@ -1,6 +1,8 @@
 package conatus.domain.posts;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface PostsRepository extends JpaRepository<Posts, Long> {
+@RepositoryRestResource(collectionResourceRel = "post", path = "post")
+public interface PostsRepository extends PagingAndSortingRepository<Posts, Long> {
 }
