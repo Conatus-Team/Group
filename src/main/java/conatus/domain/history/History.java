fc3628d80b1c79c0deafe628b9a97e431e325720
silver <silver36c@gmail.com>
2022-08-01 05:15:30 +0900

    FEAT: history 엔티티 컬럼 추가

--- src/main/java/conatus/domain/history/History.java
+++ src/main/java/conatus/domain/history/History.java
@@ -23,14 +23,18 @@ public class History extends BaseTimeEntity {
     private Long postId = (long) 0;
     private String keyword;
     private String category;
+    private Boolean isLiked = Boolean.FALSE;
+    private Boolean isClicked = Boolean.FALSE;
 
     @Builder
-    public History(Long userId, Long groupId, Long postId, String keyword, String category){
+    public History(Long userId, Long groupId, Long postId, String keyword, String category, Boolean isLiked, Boolean isClicked){
         this.userId = userId;
         this.groupId = groupId;
         this.postId = postId;
         this.keyword = keyword;
         this.category = category;
+        this.isLiked = isLiked;
+        this.isClicked = isClicked;
     }
 
     public void updateCount(){
