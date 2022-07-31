4cb475fc77c1118af365eb18e0e130f846ef5366
silver <silver36c@gmail.com>
2022-08-01 02:57:09 +0900

    FEAT: BaseTimeEntity 설정

--- src/main/java/conatus/domain/BaseTimeEntity.java
+++ src/main/java/conatus/domain/BaseTimeEntity.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -14,9 +14,30 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+    @Column(name = "created_time", nullable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @CreatedDate
     private LocalDateTime createdDate;
 
+
+    @Column(name = "updated_time", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    private void beforeSaving() {
+        this.createdDate = LocalDateTime.now();
+        System.out.println("createdDate = " + createdDate);
+//        createdBy = Thread.currentThread().getName(); //todo: put your logic here
+    }
+
+    @PreUpdate
+    private void beforeUpdating() {
+        this.updatedDate = LocalDateTime.now();
+        System.out.println("updatedDate = " + updatedDate);
+//        modifiedBy = Thread.currentThread().getName(); //todo: put your logic here
+    }
+
+
 }
