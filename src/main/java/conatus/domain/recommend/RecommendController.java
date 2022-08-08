856049616a3ba05a4fe1bab62ce48a7494ccee67
LeeJE20 <silver36c@gmail.com>
2022-08-08 17:04:36 +0900

    FEAT: 그룹 추천

--- src/main/java/conatus/domain/recommend/RecommendController.java
+++ src/main/java/conatus/domain/recommend/RecommendController.java
@@ -1,23 +1,30 @@
 package conatus.domain.recommend;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import conatus.domain.info.dto.InfoDto;
 
 @RequiredArgsConstructor
 @RestController
 public class RecommendController {
 
-//    private final PostsService postsService;
+    private final RecommendService recommendService;
 //
 //    @PostMapping("/api/v1/posts")
 //    public Long save(@RequestBody PostsSaveRequestDto requestDto){
 //        return postsService.save(requestDto);
 //    }
 //
-//    @PutMapping("/api/v1/posts/{id}")
-//    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
-//        return postsService.update(id, requestDto);
-//    }
+    @GetMapping("/group/recommend")
+    public List<InfoDto> getByUserId(@PathVariable Long userId){
+        return recommendService.getInfoByUserId(userId);
+    }
 //    @GetMapping("/api/v1/posts/{id}")
 //    public PostsResponseDto findById(@PathVariable Long id){
 //        return postsService.findById(id);
