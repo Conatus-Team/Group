64bf73e2438e270ad09140848c0d028e74f0f7c3
silver <silver36c@gmail.com>
2022-08-26 01:21:35 +0900

    FEAT: 스웨거 문서 이름 변경, 함수 설명 추가

--- src/main/java/conatus/domain/info/InfoController.java
+++ src/main/java/conatus/domain/info/InfoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import conatus.domain.info.dto.InfoDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +19,13 @@ public class InfoController {
     private final InfoRepository groupRepository;
     private final InfoService infoService;
 
+    @ApiOperation(value = "그룹 디테일")
     @GetMapping("/{id}")
     public InfoDto getById(@PathVariable Long id) {
         return infoService.getById(id);
     }
-    
+
+    @ApiOperation(value = "그룹 검색")
     @PostMapping("/search")
     public List<InfoDto> searchInfo(String keyword) {
         return infoService.search(keyword);
